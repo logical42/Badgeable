1999f56e12926edd795ddc90b57d81794c8526eb
Han <hkfortytwo@gmail.com>
2011-09-26 17:05:59 -0700

    generators

---
+++ lib/generators/badgeable/install/install_generator.rb
@@ -0,0 +1,18 @@
+require 'generators/badgeable'
+require 'rails/generators'
+require 'rails/generators/migration'
+
+module Badgeable
+  module Generators
+    class VoteGenerator < Base
+      include Rails::Generators::Migration
+      argument :user_model, :type => :string, :default => 'user', :banner => 'user_name'
+      def create_badge_models
+        generate("migration", "Badge name:string description:text icon:string")
+        generate("migration", "Badging badgeable_id:integer badgeable_type:string badge_id:integer")
+        
+      end
+
+    end
+  end
+end
