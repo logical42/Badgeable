c2182de30ccbe7c169f8eff0e2bcb76c39b40c01
Han <hkfortytwo@gmail.com>
2011-09-26 17:15:27 -0700

    added generator to initializers`

--- lib/badgeable/railtie.rb
+++ lib/badgeable/railtie.rb
@@ -1,7 +1,12 @@
+require 'rails/generators'
+
 module Badgeable
   class Railtie < ::Rails::Railtie
+    include Rails::Generators
     # AK - set the directory where badges description will appear
     initializer "badgeable.assign_badge_definitions_file" do
+      
+      create_file "lib/badges.rb"
       Badgeable::Config.badge_definitions = Pathname.new("#{Rails.root}/lib/badges.rb")
     end
     config.to_prepare do
