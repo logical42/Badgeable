c63e99979c18d466e4562faea4f78071a1b347a0
Han <hkfortytwo@gmail.com>
2011-09-26 17:09:39 -0700

    lib/badges.rb

--- lib/generators/badgeable/install/install_generator.rb
+++ lib/generators/badgeable/install/install_generator.rb
@@ -8,6 +8,7 @@ module Badgeable
       include Rails::Generators::Migration
       argument :user_model, :type => :string, :default => 'user', :banner => 'user_name'
       def create_badge_models
+        create_file "lib/badges.rb"
         generate("migration", "Badge name:string description:text icon:string")
         generate("migration", "Badging badgeable_id:integer badgeable_type:string badge_id:integer")
         
