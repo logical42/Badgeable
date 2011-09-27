56ba38267d03cb3071e2d49ce8129f3306602cf9
Han <hkfortytwo@gmail.com>
2011-09-26 18:16:22 -0700

    removed generators

--- lib/badgeable/railtie.rb
+++ lib/badgeable/railtie.rb
@@ -4,10 +4,6 @@ module Badgeable
   class Railtie < ::Rails::Railtie
     include Rails::Generators
     # AK - set the directory where badges description will appear
-    generators do
-      require "generators/badgeable/install"
-      create_file "lib/badges.rb"
-    end
     initializer "badgeable.assign_badge_definitions_file" do
       
       Badgeable::Config.badge_definitions = Pathname.new("#{Rails.root}/lib/badges.rb")
